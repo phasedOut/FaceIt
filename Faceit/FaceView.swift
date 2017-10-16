1121ed7d470c0a47291d0b638857d3dd338eaae6
ysakai <yujisakai10@gmail.com>
2017-10-16 23:17:35 +0900

    first commit

---
+++ Faceit/FaceView.swift
@@ -0,0 +1,114 @@
+//
+//  FaceView.swift
+//  Faceit
+//
+//  Created by Yuji Sakai on 2017/10/15.
+//  Copyright © 2017 Yuji Sakai. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class FaceView: UIView {
+    
+    @IBInspectable
+    var scale: CGFloat = 0.9
+    
+    @IBInspectable
+    var eyesOpen: Bool = false
+    
+    @IBInspectable
+    var mouthCurvature: Double = -1.0 // 1.0 is full smile and -1.0 is full frown
+    
+    @IBInspectable
+    var lineWidth: CGFloat = 5.0
+    
+    @IBInspectable
+    var color: UIColor = UIColor.blue
+    
+    private var skullRadius: CGFloat {
+        return min(bounds.size.width, bounds.size.height) / 2 * scale
+    }
+    private var skullCenter: CGPoint {
+        return CGPoint(x: bounds.midX, y: bounds.midY)
+    }
+    
+    private enum Eye {
+        case left
+        case right
+    }
+    
+    private func pathForMouth() -> UIBezierPath {
+        let mouthWidth = skullRadius / Ratios.skullRadiusToMouthWidth
+        let mouthHeight = skullRadius / Ratios.skullRadiustoMouthHeight
+        let mouthOffset = skullRadius / Ratios.skullRadiusToMouthOffset
+        
+        let mouthRect = CGRect(x: skullCenter.x - mouthWidth / 2,
+                               y: skullCenter.y + mouthOffset,
+                               width: mouthWidth,
+                               height: mouthHeight)
+        
+        let smileOffset = CGFloat(max(-1, min(mouthCurvature, 1))) * mouthRect.height
+        
+        let start = CGPoint(x: mouthRect.minX, y: mouthRect.midY)
+        let end = CGPoint(x: mouthRect.maxX, y: mouthRect.midY)
+        
+        let cp1 = CGPoint(x: start.x + mouthRect.width / 3 , y: start.y + smileOffset)
+        let cp2 = CGPoint(x: end.x - mouthRect.width / 3 , y: start.y + smileOffset)
+
+        let path = UIBezierPath()
+        path.move(to: start)
+        path.addCurve(to: end, controlPoint1: cp1, controlPoint2: cp2)
+        path.lineWidth = lineWidth
+        return path
+    }
+    
+    private func pathForEye(_ eye: Eye) -> UIBezierPath {
+        func centerOfEye(_ eye: Eye) -> CGPoint {
+            let eyeOffset = skullRadius / Ratios.skullRadiusToEyeOffset
+            var eyeCenter = skullCenter
+            eyeCenter.y -= eyeOffset
+            eyeCenter.x += ((eye == .left) ? -1 : 1) * eyeOffset
+            return eyeCenter
+        }
+        let eyeRadius = skullRadius / Ratios.skullRadiusToEyeRadius
+        let eyeCenter = centerOfEye(eye)
+        
+        let path: UIBezierPath
+        if eyesOpen {
+            path = UIBezierPath(arcCenter: eyeCenter, radius: eyeRadius, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: false)
+        } else {
+            path = UIBezierPath()
+            path.move(to: CGPoint(x: eyeCenter.x - eyeRadius, y: eyeCenter.y))
+            path.addLine(to: CGPoint(x: eyeCenter.x + eyeRadius, y: eyeCenter.y))
+        }
+        path.lineWidth = lineWidth
+        return path
+    }
+    
+    private func pathForSkull() -> UIBezierPath {
+        let path = UIBezierPath(arcCenter: skullCenter, radius: skullRadius, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: false)
+        path.lineWidth = lineWidth
+        return path
+    }
+
+    override func draw(_ rect: CGRect) {
+
+        
+        color.set()
+        pathForSkull().stroke()
+        pathForEye(Eye.left).stroke()
+        pathForEye(Eye.right).stroke()
+        pathForMouth().stroke()
+    }
+    
+    private struct Ratios {
+        static let skullRadiusToEyeOffset: CGFloat = 3
+        static let skullRadiusToEyeRadius: CGFloat = 10
+        static let skullRadiusToMouthWidth: CGFloat = 1
+        static let skullRadiustoMouthHeight: CGFloat = 3
+        static let skullRadiusToMouthOffset: CGFloat = 3
+    }
+
+
+}
---
+++ Faceit/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Faceit
+//
+//  Created by Yuji Sakai on 2017/10/15.
+//  Copyright © 2017 Yuji Sakai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+}
+
