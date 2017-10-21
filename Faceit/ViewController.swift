51d6243921a64b5c0b91554047c76bf823cd39c3
ysakai <yujisakai10@gmail.com>
2017-10-21 18:56:11 +0900

    lecture

--- Faceit/ViewController.swift
+++ Faceit/ViewController.swift
@@ -10,5 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    //model
+    @IBOutlet weak var faceView: FaceView! {
+        didSet {
+            //update when model is set (will be called only once)
+            updateUI()
+        }
+    }
+    
+    //view
+    var expression = FacialExpression(eyes: .open, mouth: .grin) {
+        didSet {
+            //update every time view changes
+            updateUI()
+        }
+    }
+    
+    //update model when view changes
+    private func updateUI() {
+        //one way to set model
+        switch expression.eyes {
+        case .open:
+            faceView?.eyesOpen = true
+        case .closed:
+            faceView?.eyesOpen = false
+        case .squinting:
+            faceView?.eyesOpen = false
+        }
+        //this way is possible too
+        faceView?.mouthCurvature = mouthCurvatures[expression.mouth] ?? 0
+    }
+    
+    private let mouthCurvatures = [
+        FacialExpression.Mouth.grin:0.5,
+        .frown:-1.0,
+        .smile:1.0,
+        .neutral:0,
+        .smirk:-0.5
+    ]
+    
+
 }
 
